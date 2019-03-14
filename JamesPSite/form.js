32a1e92461ac2441dc86f315a5e1e2ec1a0b31a4
jrice22 <31262838+jrice22@users.noreply.github.com>
2019-03-14 06:00:22 -0600

    Add files via upload

--- JamesPSite/form.js
+++ JamesPSite/form.js
@@ -1,63 +1,51 @@
+$(document).ready(function() {
+  $("#submit_btn").click(function() {
 
-$(function()
-{
-    function after_form_submitted(data) 
+    var proceed = true;
+    //simple validation at client's end
+    //loop through each field and we simply change border color to red for invalid fields       
+    $("#contact_form input[required=true], #contact_form textarea[required=true]").each(function() {
+      $(this).css('border-color', '');
+      if (!$.trim($(this).val())) { //if this field is empty 
+        $(this).css('border-color', 'red'); //change border color to red   
+        proceed = false; //set do not proceed flag
+      }
+      //check invalid email
+      var email_reg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+      if ($(this).attr("type") == "email" && !email_reg.test($.trim($(this).val()))) {
+        $(this).css('border-color', 'red'); //change border color to red   
+        proceed = false; //set do not proceed flag              
+      }
+    });
+
+    if (proceed) //everything looks good! proceed...
     {
-        if(data.result == 'success')
-        {
-            $('form#reused_form').hide();
-            $('#success_message').show();
-            $('#error_message').hide();
+      //get input field values data to be sent to server
+      post_data = {
+        'user_name': $('input[name=name]').val(),
+        'user_email': $('input[name=email]').val(),
+        'phone_number': $('input[name=phone2]').val(),
+        'msg': $('textarea[name=message]').val()
+      };
+
+      //Ajax post data to server
+      $.post('php/contact_me.php', post_data, function(response) {
+        if (response.type == 'error') { //load json data from server and output message     
+          output = '<div class="error">' + response.text + '</div>';
+        } else {
+          output = '<div class="success">' + response.text + '</div>';
+          //reset values in all input fields
+          $("#contact_form  input[required=true], #contact_form textarea[required=true]").val('');
+          $("#contact_form #contact_body").slideUp(); //hide form after success
         }
-        else
-        {
-            $('#error_message').append('<ul></ul>');
-
-            jQuery.each(data.errors,function(key,val)
-            {
-                $('#error_message ul').append('<li>'+key+':'+val+'</li>');
-            });
-            $('#success_message').hide();
-            $('#error_message').show();
-
-            //reverse the response on the button
-            $('button[type="button"]', $form).each(function()
-            {
-                $btn = $(this);
-                label = $btn.prop('orig_label');
-                if(label)
-                {
-                    $btn.prop('type','submit' ); 
-                    $btn.text(label);
-                    $btn.prop('orig_label','');
-                }
-            });
-            
-        }//else
+        $("#contact_form #contact_results").hide().html(output).slideDown();
+      }, 'json');
     }
+  });
 
-	$('#reused_form').submit(function(e)
-      {
-        e.preventDefault();
-
-        $form = $(this);
-        //show some response on the button
-        $('button[type="submit"]', $form).each(function()
-        {
-            $btn = $(this);
-            $btn.prop('type','button' ); 
-            $btn.prop('orig_label',$btn.text());
-            $btn.text('Sending ...');
-        });
-        
-
-                    $.ajax({
-                type: "POST",
-                url: 'handler.php',
-                data: $form.serialize(),
-                success: after_form_submitted,
-                dataType: 'json' 
-            });        
-        
-      });	
+  //reset previously set border colors and hide all message on .keyup()
+  $("#contact_form  input[required=true], #contact_form textarea[required=true]").keyup(function() {
+    $(this).css('border-color', '');
+    $("#result").slideUp();
+  });
 });
